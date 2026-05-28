e8c77558717183a3973be15660c0d9c7f525e65b
sugu <sugukumar778@gmail.com>
2026-05-28 13:04:33 +0530

    Clean up unused debug and temporary scripts

--- backend/read_debug.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-try {
-    const data = fs.readFileSync('debug_output.txt', 'utf16le');
-    console.log(data);
-} catch (err) {
-    console.error(err);
-}
-process.exit(0);
